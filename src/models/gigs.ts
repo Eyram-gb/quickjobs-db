a049daca1f2fd8d21a2c1d7741ce571d2b85c597
eyram <pistisray@gmail.com>
2024-08-03 22:30:20 +0000

    initial users table and gigs table

---
+++ src/models/gigs.ts
@@ -0,0 +1,35 @@
+import {
+  boolean,
+  decimal,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { employer_profile, users } from "./schema";
+
+export const gigs = pgTable("gigs", {
+  id: uuid("id").defaultRandom().primaryKey().unique(),
+  user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade" }),
+  empoyer_id: uuid("empoyer_id").references(() => employer_profile.id, {
+    onDelete: "cascade",
+  }),
+  title: varchar("title"),
+  description: varchar("description"),
+  duration: varchar("duration"),
+  location: varchar("location"),
+  budget_range: decimal("budget_range").notNull(),
+  created_at: timestamp("created_at", {
+    precision: 0,
+    withTimezone: true,
+  }).defaultNow(),
+  requirements: text("requirements"),
+  updated_at: timestamp("updated_at", {
+    precision: 0,
+    withTimezone: true,
+  }).$onUpdate(() => new Date()),
+  is_active: boolean("is_active").default(true),
+  is_deleted: boolean("is_deleted").default(false),
+  //   is_featured: boolean("is_featured").default(false),
+});
--- src/models/schema/users.ts
+++ src/models/schema/users.ts
@@ -1,5 +1,48 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
+export const userEnum = pgEnum("user_enum", ["admin", "client", "company"]);
 export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey().unique(),
+  email: varchar("email").unique().unique(),
+  password_hash: varchar("password_hash").notNull(),
+  user_type: userEnum("user_type").notNull(),
+  created_at: timestamp("created_at", {
+    precision: 0,
+    withTimezone: true,
+  }).defaultNow(),
+  updated_at: timestamp("updated_at", {
+    precision: 0,
+    withTimezone: true,
+  }).$onUpdate(() => new Date()),
+});
+
+export const employer_profile = pgTable("employer_profile", {
+  id: uuid("id").defaultRandom().primaryKey().unique(),
+  user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade" }),
+  name: varchar("name").notNull(),
+  logo_url: varchar("logo_url"),
+  location: varchar("location"),
+  website_url: varchar("website_url"),
+  industry: varchar("industry"),
+});
+
+export const applicant_profile = pgTable("applicant_profile", {
+  id: uuid("id").defaultRandom().primaryKey().unique(),
+  user_id: uuid("user_id").references(() => users.id, { onDelete: "cascade" }),
+  first_name: varchar("first_name").notNull(),
+  last_name: varchar("last_name").notNull(),
+  bio: varchar("bio"),
+  resume_url: varchar("resume_url"),
+  skills: varchar("skills"),
+  location: varchar("location"),
+  education: varchar("education"),
+  experience: varchar("experience"),
+  created_at: timestamp("created_at", {
+    precision: 0,
+    withTimezone: true,
+  }).defaultNow(),
+  updated_at: timestamp("updated_at", {
+    precision: 0,
+    withTimezone: true,
+  }).$onUpdate(() => new Date()),
 });
