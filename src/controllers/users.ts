e89d0914d58791dde8e57675cc5fedefeb8d550a
eyram <pistisray@gmail.com>
2024-08-05 00:11:21 +0000

    login controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
-import { insertNewUser } from "../models/queries/users";
+import { findUserByEmail, insertNewUser } from "../models/queries/users";
 // import { NewUser } from "../models/schema";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { NewUserSchema } from "../types/zod-validations/user";
 import logger from "../lib/logger";
 
@@ -20,3 +21,41 @@ export const createNewUser = async (req: Request, res: Response) => {
     return logger.error(error)
   }
 };
+
+export const loginUser = async (req:Request, res:Response) => {
+   try {
+     const password = await req.body.password;
+     const email = await req.body.email;
+     const [user] = await findUserByEmail(email);
+     if (!user) {
+       return res.status(404).json({
+         message: "User not found",
+       });
+     }
+
+     const isMatch = await bcrypt.compare(password, user.password_hash);
+     if (!isMatch) {
+       return res.status(400).json({
+         message: "Wrong password",
+       });
+     }
+
+     const token = jwt.sign(
+       { email: user.email, password: user.password_hash },
+       process.env.ACCESS_TOKEN_SECRET || ""
+     );
+     if (token) {
+       return res.status(201).json({
+         message: "login succesful",
+         token,
+         user,
+       });
+     }
+   } catch (error) {
+     console.error(error);
+     return res.status(500).json({
+       message: "User login failed.",
+       error,
+     });
+   }
+}
--- src/models/queries/users.ts
+++ src/models/queries/users.ts
@@ -1,7 +1,11 @@
+import { eq } from "drizzle-orm";
 import { db } from "../../lib/db";
 import { NewUser, users } from "../schema";
 
-export async function insertNewUser (user: NewUser) {
-     return await db.insert(users).values(user).returning();
-     
+export async function insertNewUser(user: NewUser) {
+  return await db.insert(users).values(user).returning();
+}
+
+export async function findUserByEmail(email: string) {
+  return await db.select().from(users).where(eq(users.email, email));
 }
