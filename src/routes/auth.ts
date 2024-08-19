83b9c0378b9b9624005a0acbbe1a918b6c45db95
eyram <pistisray@gmail.com>
2024-08-19 16:24:49 +0000

    add middleware to auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { loginUser, registerNewUser } from "../controllers/auth";
+import { Verify } from "../middleware/verify";
 
 const router = Router();
 
 router.post("/auth/register", registerNewUser);
-router.post("/auth/login", loginUser);
-router.post("/auth/logoout", registerNewUser);
+router.post("/auth/login", Verify, loginUser);
+router.post("/auth/logout", Verify, registerNewUser);
 
 export const authRoutes = router;
