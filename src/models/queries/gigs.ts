1e06101652a5435f6713cf64f22f1361d953d10f
eyram <pistisray@gmail.com>
2024-09-30 12:42:43 +0000

    get gigs query update

--- src/models/queries/gigs.ts
+++ src/models/queries/gigs.ts
@@ -1,9 +1,35 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../lib/db";
 import { gigs, TGig } from "../schema/gigs";
+import { employer_profile } from "../schema";
+
+// Define a new type that includes all fields from gigs and logo_url from employer_profile
+export type GigWithEmployerLogo = TGig & {
+  logo_url: string | null; // Adjust type based on your schema
+};
 
 export async function findAllGigs() {
-  return await db.select().from(gigs);
+  return await db
+    .select({
+      id: gigs.id,
+      user_id: gigs.user_id,
+      employer_id: gigs.employer_id,
+      title: gigs.title,
+      description: gigs.description,
+      duration: gigs.duration,
+      location: gigs.location,
+      budget_range: gigs.budget_range,
+      requirements: gigs.requirements,
+      industry_id: gigs.industry_id,
+      created_at: gigs.created_at,
+      updated_at: gigs.updated_at,
+      is_active: gigs.is_active,
+      is_deleted: gigs.is_deleted,
+      company_logo: employer_profile.logo_url,
+      company_name: employer_profile.name
+    })
+    .from(gigs)
+    .innerJoin(employer_profile, eq(employer_profile.id, gigs.employer_id));
 }
 
 export async function findGigById(id: string) {
@@ -14,14 +40,18 @@ export async function createGig(gigBody: TGig) {
   return await db.insert(gigs).values(gigBody).returning();
 }
 
-export async function editGig(id: string, gigBody: TGig ) {
-    return await db.update(gigs).set(gigBody).where(eq(gigs.id, id)).returning();
+export async function editGig(id: string, gigBody: TGig) {
+  return await db.update(gigs).set(gigBody).where(eq(gigs.id, id)).returning();
 }
 
 export async function removeGig(id: string) {
-  return await db.update(gigs).set({ is_deleted: true }).where(eq(gigs.id, id)).returning();
+  return await db
+    .update(gigs)
+    .set({ is_deleted: true })
+    .where(eq(gigs.id, id))
+    .returning();
 }
 
-export async function findGigsByEmployerId(id:string){
-  return await db.select().from(gigs).where(eq(gigs.employer_id, id))
+export async function findGigsByEmployerId(id: string) {
+  return await db.select().from(gigs).where(eq(gigs.employer_id, id));
 }
